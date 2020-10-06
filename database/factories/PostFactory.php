cb2d6b6616a48d987d53ef0d862049065dc572c7
Guido <molido@hotmail.it>
2020-10-06 15:27:26 +0200

    tabella post

--- database/factories/PostFactory.php
+++ database/factories/PostFactory.php
@@ -6,8 +6,11 @@ use App\Model;
 use App\Post;
 use Faker\Generator as Faker;
 
-$factory->define(Model::class, function (Faker $faker) {
+$factory->define(Post::class, function (Faker $faker) {
     return [
-        //
+      'title'   => $faker -> word(),
+      'topic'   => $faker -> sentence($nbWords = 4, $variableNbWords = true),
+      'content' => $faker -> text($maxNbChars = 200),
+      'views'   => $faker -> numberBetween($min = 1, $max = 9000)
     ];
 });
